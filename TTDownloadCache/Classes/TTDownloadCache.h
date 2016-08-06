7d3e5676a4367eab3050e643fc573dfa9823f743
Dhiraj Gupta <dhiraj@traversient.com>
2016-08-06 20:44:38 +0530

    Added empty main download cache class.

---
+++ TTDownloadCache/Classes/TTDownloadCache.h
@@ -0,0 +1,13 @@
+//
+//  TTDownloadCache.h
+//  Pods
+//
+//  Created by Dhiraj Gupta on 8/6/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTDownloadCache : NSObject
+
+@end
---
+++ TTDownloadCache/Classes/TTDownloadCache.m
@@ -0,0 +1,13 @@
+//
+//  TTDownloadCache.m
+//  Pods
+//
+//  Created by Dhiraj Gupta on 8/6/16.
+//
+//
+
+#import "TTDownloadCache.h"
+
+@implementation TTDownloadCache
+
+@end
