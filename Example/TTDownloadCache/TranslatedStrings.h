3be45c5b0a6b414c809eacc572a1281d5017e4ae
Dhiraj Gupta <dhiraj@traversient.com>
2016-08-06 19:41:46 +0530

    Added basic utility classes and translated strings.

---
+++ Example/TTDownloadCache/TranslatedStrings.h
@@ -0,0 +1,19 @@
+//
+//  TranslatedStrings.h
+//  Uniconnect
+//
+//  Created by Dhiraj Gupta on 7/12/16.
+//  Copyright © 2016 Uniconnect. All rights reserved.
+//
+
+#ifndef TranslatedStrings_h
+#define TranslatedStrings_h
+
+#define S_Okay NSLocalizedString(@"Ok",@"")
+#define S_Cancel NSLocalizedString(@"Cancel",@"")
+#define S_Search NSLocalizedString(@"Search",@"")
+#define S_Sorry NSLocalizedString(@"Sorry",@"")
+#define S_Confirm NSLocalizedString(@"Confirm",@"")
+#define S_Sent NSLocalizedString(@"Sent",@"")
+
+#endif /* TranslatedStrings_h */
