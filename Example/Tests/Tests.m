4ae0a9c7cc578be35997112f6ae4a9e9e43c2436
Dhiraj Gupta <dhiraj@traversient.com>
2016-08-07 21:44:32 +0530

    Added basic test cases.

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -8,38 +8,39 @@
 
 // https://github.com/Specta/Specta
 
-SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
+SpecBegin(DownloadCache)
 
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
+describe(@"Basic", ^{
 
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
+    __block TTDownloadCache * cache = nil;
+    __block NSString * jsonURL = nil;
+    beforeAll(^{
+        cache = [[TTDownloadCache alloc] initWithMaxSize:20000 * 2];
+        jsonURL = @"https://api.flickr.com/services/rest/?extras=url_q%2Cowner_name%2Clast_update%2Co_dims&format=json&method=flickr.interestingness.getList&per_page=10&nojsoncallback=1&page=1&api_key=0845bbb79445bf10a0a1ea948aa5dae7";
     });
     
-    it(@"will wait for 10 seconds and fail", ^{
+    it(@"should not be nil", ^{
+        XCTAssertNotNil(cache);
+    });
+    
+    it(@"can download JSON", ^{
         waitUntil(^(DoneCallback done) {
-        
+            [cache dataFromURL:jsonURL withHandler:^(NSData *data, NSString * request, BOOL fromCache) {
+                if(data != nil){
+                    done();
+                }
+            } useMemCache:YES];
+            
         });
     });
-});
-
-describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
-    });
-    
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^{
+    it(@"JSON can be cached", ^{
         waitUntil(^(DoneCallback done) {
-            done();
+            [cache dataFromURL:jsonURL withHandler:^(NSData *data, NSString * request, BOOL fromCache) {
+                if(data != nil && fromCache){
+                    done();
+                }
+            } useMemCache:YES];
         });
     });
 });
